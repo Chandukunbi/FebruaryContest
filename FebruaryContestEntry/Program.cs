12a5f0de73ddf89d0ab4cea8e9663a5b97269d2f
Chandukunbi <chandukunbi@gmail.com>
2020-02-20 10:52:54 +0530

    Implemented code

--- FebruaryContestEntry/Program.cs
+++ FebruaryContestEntry/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace FebruaryContestEntry
 {
@@ -8,12 +9,33 @@ namespace FebruaryContestEntry
 
 		static void Main(string[] args)
 		{
-			// The solution implementation goes here
+            // The solution implementation goes here
+
+            for (int row = 1; row <= 15; row++)
+            {
+
+                for (int col = 1; col <= 30; col++)
+                {
+                    if (row == 1 || row == 15 || col == 1 || col == 30)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;                        
+                        Console.Write(block);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(block);
+                    }
+                }
+
+                Console.WriteLine();
+            }
+            
 
 			Console.ResetColor();
 			Console.WriteLine();
 			Console.WriteLine();
-			Console.WriteLine("Yay, a colorful shape!");
+			//Console.WriteLine("Yay, a colorful shape!");
 			Console.ReadLine();
 		}
 	}
